71ff73e6395441ca96c9c26b008629c3f3d9d600
Jusuf Osmanovic <jusuf.osmanovic01@gmail.com>
2026-04-27 02:52:34 +0300

    filter image search only

--- src/gui.py
+++ src/gui.py
@@ -1,4 +1,22 @@
 import tkinter as tk
+from tkinter.filedialog import askopenfilename
+from pathlib import Path
+
+def on_select():
+    image_path = askopenfilename(
+        title="Choose an image", 
+        filetypes=[("Image files", "*.png")]
+    )
+    if not image_path:
+        print("No file selected")
+        return
+    
+    if Path(image_path).suffix.lower() not in {
+         ".png", ".jpg", ".jpeg", ".bmp", ".gif", ".tiff"}:
+            print ("Not an image file")
+            return
+    
+    print(image_path)
 
 def run_app():
     window = tk.Tk()
@@ -11,11 +29,11 @@ def run_app():
     text2 = tk.Label(window, text=text_instruction, font=("Arial", 16))
 
     text_button = "Select"
-    select_button = tk.Button(window, text=text_button,)
+    select_button = tk.Button(window, text=text_button, command=on_select)
     
     text1.pack(pady=20)
     text2.pack(pady=20)
     select_button.pack(pady=20)
 
     window.mainloop()
-    return "yes it works"
+    return "No errors"
