e2597c0cdc4bf0be7bf0ef125102e0a2688e3301
Jusuf Osmanovic <jusuf.osmanovic01@gmail.com>
2026-04-28 02:33:01 +0300

    check size difference in orginal and new text

--- tests/test_pdf_writer.py
+++ tests/test_pdf_writer.py
@@ -4,9 +4,9 @@ from src.ocr import extract_text_data
 from src.pdf_writer import image_to_pdf
 
 def test_image_to_pdf():
-    image_path = "tests/images/letter.webp"
-    output_path = "tests/output/letter_out.pdf"
-    
+    image_path = "tests/images/plain-text.png"
+    output_path = "tests/output/plain-text.pdf"
+
     words = extract_text_data(image_path)
     result = image_to_pdf(image_path, words, output_path)
     assert Path(result).exists()
