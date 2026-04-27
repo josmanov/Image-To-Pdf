b5a0f25e8670db18ba27a7ca3df1feb11879a0cd
Jusuf Osmanovic <jusuf.osmanovic01@gmail.com>
2026-04-28 01:23:05 +0300

    get position data from detected words

--- src/ocr.py
+++ src/ocr.py
@@ -1,8 +1,25 @@
 from PIL import Image
 import pytesseract
 
-def extract_text(image_path: str) -> str:
+def extract_text_data(image_path: str) -> str:
     image = Image.open(image_path)
-    text = pytesseract.image_to_string(image)
-    return text
+    data = pytesseract.image_to_data(image)
     
+    words = []
+    for line in data.strip().split('\n')[1:]:
+        parts = line.split('\t')
+        if len(parts) == 12:
+            level = int(parts[0])
+            x, y, width, height, conf = map(int, parts[6:11])
+            text = parts[11]
+
+            if level == 5 and text.strip():
+                words.append({
+                    'text': text,
+                    'x': x,
+                    'y': y,
+                    'width': width,
+                    'height': height,
+                    'confidence': conf
+                })
+    return words
